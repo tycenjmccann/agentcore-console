5eab98b44055cd4d04fbb715e5d2bb2aded3c3a6
tycenjmccann <43410826+tycenjmccann@users.noreply.github.com>
2026-05-21 15:05:05 -0700

    chore: add jszip dependency and export S3ArtifactsModal from index

--- src/components/workflow/index.ts
+++ src/components/workflow/index.ts
@@ -3,6 +3,7 @@ export { default as PhaseBox } from "./PhaseBox";
 export { default as AgentItem } from "./AgentItem";
 export { default as SVGConnector } from "./SVGConnector";
 export { default as AgentOutputPanel } from "./AgentOutputPanel";
+export { default as S3ArtifactsModal } from "./S3ArtifactsModal";
 export { default as StatusBar } from "./StatusBar";
 export { usePipelineSSE } from "./usePipelineSSE";
 export { MarkdownRenderer } from "./MarkdownRenderer";
